ca680eca3c8458c1ebce364d3f7bd4850810ad6e
Renato Reyes Valenzuela <u20221b471@upc.edu.pe>
2024-06-06 13:01:51 -0500

    Primera Parte Renato Reyes

---
+++ js/funciones.js
@@ -0,0 +1,7 @@
+//function btnRegistrarse(){
+    //window.location.assign('formRegistro.html'); //redirige pero no carga
+    //console.log("Redirigiendo a formRegistro.html");
+//    window.open("http://www.google.com/","_self");
+    //window.open("formRegistro.html","_self");
+//}
+
